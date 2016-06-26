b7715e1d0d58990f9c26123bb6a22024be44a9a9
Ibrahim1414 <owf@windowslive.com>
2016-06-26 15:42:18 +0300

    Add files via upload

---
+++ HelloWorld/src/main/java/com/example/HelloWorldApplication.java
@@ -0,0 +1,22 @@
+package com.example;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+
+
+@Controller
+public class HelloWorldApplication {
+	
+
+@RequestMapping(value="", method= RequestMethod.GET)
+public String HelloWorld(ModelMap model) {
+	
+	return "HelloWorld";
+	
+}
+	
+}
+
